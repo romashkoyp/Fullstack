70a817e8d231e7efe66ec9681f690c6f73722e9a
Yaroslav <romashkoyp@gmail.com>
2024-01-11 10:41:35 +0200

    Phonebook updated

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.persons)
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
   
   const addName = (event) => {
     event.preventDefault()
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,16 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App'
-
-const persons = [
-  {
-    name: 'Arto Hellas',
-    number: 121234421
-  },
-  {
-    name: 'Second Name',
-    number: 121234232
-  },
-]
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons} />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
