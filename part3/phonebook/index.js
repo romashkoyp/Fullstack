fe67fa4926019d4c1c78454f7c43a0fd07e04439
Yaroslav <romashkoyp@gmail.com>
2024-01-19 11:24:45 +0200

    Update

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data', {
