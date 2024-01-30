5041f9ff80585235152e6748878ba8614a02ea58
Yaroslav <romashkoyp@gmail.com>
2024-01-30 09:05:35 +0200

    update

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,152 +14,161 @@ beforeEach(async () => {
   }
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('Blog API', () => {
+  describe('Blog retrieval', () => {
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    }, 100000)
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+    test('all blogs are returned', async () => {
+      const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+      expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
 
-test('the first blog is about database', async () => {
-  const response = await api.get('/api/blogs')
+    test('the first blog is about database', async () => {
+      const response = await api.get('/api/blogs')
 
-  expect(response.body[0].title).toBe('Database')
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Morgan Freeman',
-    _url: 'https://cloud.mongodb.com/',
-    likes: 1
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(
-    'async/await simplifies making async calls'
-  )
-})
-
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: 'Morgan Freeman',
-    _url: 'https://cloud.mongodb.com/'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blog without url is not added', async () => {
-  const newBlog = {
-    author: 'Morgan Freeman',
-    title: 'New title'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blog posts have an id property', async () => {
-  const response = await api.get('/api/blogs')
-  const blogs = response.body
-
-  blogs.forEach((blog) => {
-    expect(blog.id).toBeDefined()
-    expect(blog._id).toBeUndefined()
+      expect(response.body[0].title).toBe('Database')
+    })
   })
-})
 
-test('likes is equal to 0 if no likes in request', async () => {
-  const newBlog = {
-    title: 'new title',
-    author: 'Morgan Freeman the Second',
-    _url: 'https://cloud.mongodb.com/'
-  }
+  describe('Blog Modification', () => {
+    describe('Blog validation', () => {
+      test('a valid blog can be added', async () => {
+        const newBlog = {
+          title: 'async/await simplifies making async calls',
+          author: 'Morgan Freeman',
+          _url: 'https://cloud.mongodb.com/',
+          likes: 1
+        }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  const likes = blogsAtEnd.map(n => n.likes)
-  expect(likes).toContain(0)
-})
+        const titles = blogsAtEnd.map(n => n.title)
+        expect(titles).toContain(
+          'async/await simplifies making async calls'
+        )
+      })
 
-test('delete single blog', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
+      test('blog without title is not added', async () => {
+        const newBlog = {
+          author: 'Morgan Freeman',
+          _url: 'https://cloud.mongodb.com/'
+        }
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(400)
 
-  const blogsAtEnd = await helper.blogsInDb()
+        const blogsAtEnd = await helper.blogsInDb()
 
-  expect(blogsAtEnd).toHaveLength(
-    helper.initialBlogs.length - 1
-  )
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+      })
 
-  const titles = blogsAtEnd.map(r => r.title)
+      test('blog without url is not added', async () => {
+        const newBlog = {
+          author: 'Morgan Freeman',
+          title: 'New title'
+        }
 
-  expect(titles).not.toContain(blogToDelete.title)
-})
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(400)
 
-test('update the number of likes for a blog by id', async () => {
-  const newBlog = {
-    likes: 30
-  }
+        const blogsAtEnd = await helper.blogsInDb()
 
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToUpdate = blogsAtStart[1]
-  const updatedLikes = newBlog.likes
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+      })
 
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send({ likes: updatedLikes })
-    .expect(200)
+      test('blog posts have an id property', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body
 
-  const blogsAtEnd = await helper.blogsInDb()
+        blogs.forEach((blog) => {
+          expect(blog.id).toBeDefined()
+          expect(blog._id).toBeUndefined()
+        })
+      })
+    })
+    describe('Blog insertion, deletion and update', () => {
+      test('likes is equal to 0 if no likes in request', async () => {
+        const newBlog = {
+          title: 'new title',
+          author: 'Morgan Freeman the Second',
+          _url: 'https://cloud.mongodb.com/'
+        }
 
-  expect(blogsAtEnd).toHaveLength(
-    helper.initialBlogs.length
-  )
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
 
-  const likes = blogsAtEnd.map(r => r.likes)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  expect(likes).toContain(updatedLikes)
+        const likes = blogsAtEnd.map(n => n.likes)
+        expect(likes).toContain(0)
+      })
+
+      test('delete single blog', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+          .delete(`/api/blogs/${blogToDelete.id}`)
+          .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+          helper.initialBlogs.length - 1
+        )
+
+        const titles = blogsAtEnd.map(r => r.title)
+
+        expect(titles).not.toContain(blogToDelete.title)
+      })
+
+      test('update the number of likes for a blog by id', async () => {
+        const newBlog = {
+          likes: 30
+        }
+
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToUpdate = blogsAtStart[1]
+        const updatedLikes = newBlog.likes
+
+        await api
+          .put(`/api/blogs/${blogToUpdate.id}`)
+          .send({ likes: updatedLikes })
+          .expect(200)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+          helper.initialBlogs.length
+        )
+
+        const likes = blogsAtEnd.map(r => r.likes)
+
+        expect(likes).toContain(updatedLikes)
+      })
+    })
+  })
 })
 
 afterAll(async () => {
