532c6e455387814552287858595bd2a51ce89eec
Yaroslav <romashkoyp@gmail.com>
2024-02-09 11:17:42 +0200

    5.2: bloglist frontend completed

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import logoutService from './services/logout'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -50,6 +51,21 @@ const App = () => {
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    try {
+      await logoutService.logout()
+      window.localStorage.removeItem('loggedBlogappUser')
+      blogService.setToken(null)
+      setUser(null)
+    } catch (exception) {
+      setErrorMessage('logout failed');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000)
+    }
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -88,7 +104,9 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <Notification message={errorMessage} />
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in 
+        <button type="button" onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
