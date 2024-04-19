e1fd21130bdb66a0f8f304edf0e8a9701455a7ff
Yaroslav <romashkoyp@gmail.com>
2024-04-19 15:36:23 +0300

    9.5 WebBMI

--- part9/firstProject/bmiCalculator.ts
+++ part9/firstProject/bmiCalculator.ts
@@ -1,60 +1,49 @@
-// npm run CalculBmi
+// npm run CalculBmi 180 74
 interface Values {
   height: number;
   weight: number;
 }
 
-const parseArguments = (args: string[]): Values => {
-  // console.log(args.length)
-  // console.log(args[0])
-  // console.log(args[1])
-  console.log(args[2]) //height
-  console.log(args[3]) //weight
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+export const parseArguments = (args: string[]): Values => {
+  console.log("Number of arguments:", args.length)
+  args.forEach((arg) => {console.log(arg);})
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length > 2) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[0])) && !isNaN(Number(args[1]))) {
     return {
-      height: Number(args[2]),
-      weight: Number(args[3])
+      height: Number(args[0]),
+      weight: Number(args[1])
     }
   } else {
     throw new Error('Provided values were not numbers!');
   }
 }
 
-const calculateBmi = (cm: number, kg: number) : number => {
+export const calculateBmi = (cm: number, kg: number): string => {
   if (kg === 0) throw new Error('Can\'t divide by 0!');
   if (kg < 0 ) throw new Error('Weight has to be a positive number');
   if (cm < 0 ) throw new Error('Height has to be a positive number');
-  return kg / ((cm / 100) ** 2)
-}
+  
+  const bmi = kg / ((cm / 100) ** 2);
 
-try {
-  const { height, weight } = parseArguments(process.argv)
-  const bmi = calculateBmi(height, weight);
-  console.log(`BMI is: ${bmi}`)
   if (bmi < 16) {
-    console.log('Underweight (Severe thinness)');
+    return 'Underweight (Severe thinness)';
   } else if (bmi < 16.9 && bmi > 16) {
-    console.log('Underweight (Moderate thinness)');
+    return 'Underweight (Moderate thinness)';
   } else if (bmi < 18.4 && bmi > 17) {
-    console.log('Underweight (Mild thinness)');
+    return 'Underweight (Mild thinness)';
   } else if (bmi < 24.9 && bmi > 18.5) {
-    console.log('Normal (healthy weight)');
+    return 'Normal (healthy weight)';
   } else if (bmi < 29.9 && bmi > 25) {
-    console.log('Overweight (Pre-obese)');
+    return 'Overweight (Pre-obese)';
   } else if (bmi < 34.9 && bmi > 30) {
-    console.log('Obese (Class I)');
+    return 'Obese (Class I)';
   } else if (bmi < 39.9 && bmi > 35) {
-    console.log('Obese (Class II)');
-  } else if (bmi > 40 || bmi === 40) {
-    console.log('Obese (Class II)');
+    return 'Obese (Class II)';
+  } else if (bmi >= 40) {
+    return 'Obese (Class III)';
   }
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
+  return '';
 }
--- part9/firstProject/index.ts
+++ part9/firstProject/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import { parseArguments, calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get(`/bmi`, (req, res) => {
+  try {
+    const { height, weight } = parseArguments([req.query.height as string, req.query.weight as string]);
+    const bmi = calculateBmi(height, weight);
+
+    res.status(200).json({
+      weight: weight,
+      height: height,
+      bmi: bmi
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: "malformatted parameters" });
+    } else {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 });
 
 const PORT = 3003;
