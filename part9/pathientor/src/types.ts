914a84533e461278489287e71735c859aaa8ac09
Yaroslav <romashkoyp@gmail.com>
2024-05-02 11:00:27 +0300

    9.22: Patientor, step 3

--- part9/pathientor/src/types.ts
+++ part9/pathientor/src/types.ts
@@ -2,16 +2,65 @@ export interface DiagnosesEntry {
   code: string;
   name: string;
   latin?: string;
-};
-
-export interface Entry {
 }
 
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosesEntry['code']>;
+  type?: string;
+  discharge?: DischargeEntry;
+  employerName?: string;
+  sickLeave?: SickLeaveEntry;
+  healthCheckRating?: HealthCheckRating; 
+
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface DischargeEntry {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: DischargeEntry;
+}
+
+interface SickLeaveEntry {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave: SickLeaveEntry;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
-  male = 'male',
-  female = 'female',
-  other = 'other',
-};
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
 
 export interface PatientEntry {
   id: string;
@@ -20,8 +69,8 @@ export interface PatientEntry {
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries: Entry[]
-};
+  entries: BaseEntry[]
+}
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn' | 'entries'>;
 
