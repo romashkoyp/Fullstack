303e4d6c74e1c5f4c922c5e1cd713180672e880f
Yaroslav <romashkoyp@gmail.com>
2024-05-02 11:41:02 +0300

    9.23: Patientor, step 4

--- part9/patientor-frontend/patientor/src/components/PersonalPatientPage/index.tsx
+++ part9/patientor-frontend/patientor/src/components/PersonalPatientPage/index.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import patientService from "../../services/patients";
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
-import { Patient } from '../../types';
+import { PatientEntry } from '../../types';
 
 interface PatientIdProps {
-  setPatient: React.Dispatch<React.SetStateAction<Patient | null>>;
+  setPatient: React.Dispatch<React.SetStateAction<PatientEntry | null>>;
 }
 
 const PatientId: React.FC<PatientIdProps> = ({ setPatient }) => {
-  const [patientData, setPatientData] = useState<Patient | null>(null);
+  const [patientData, setPatientData] = useState<PatientEntry | null>(null);
   const patientId = window.location.pathname.split("/")[2];
   // console.log(window.location);
 
@@ -52,6 +52,15 @@ const PatientId: React.FC<PatientIdProps> = ({ setPatient }) => {
       ) : null}</h2>
       <p>ssn: {patientData.ssn}</p>
       <p>occupation: {patientData.occupation}</p>
+      <h2>entries</h2>
+      {patientData.entries.map(entry => 
+        <div key={entry.id}>
+          <p>{entry.date} {entry.description}</p>
+          {entry.diagnosisCodes?.map(code =>
+            <li key={code}>{code}</li>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- part9/patientor-frontend/patientor/src/types.ts
+++ part9/patientor-frontend/patientor/src/types.ts
@@ -1,22 +1,77 @@
-export interface Diagnosis {
+export interface DiagnosesEntry {
   code: string;
   name: string;
   latin?: string;
 }
 
-export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosesEntry['code']>;
+  type?: string;
+  discharge?: DischargeEntry;
+  employerName?: string;
+  sickLeave?: SickLeaveEntry;
+  healthCheckRating?: HealthCheckRating; 
+
 }
 
-export interface Patient {
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface DischargeEntry {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: DischargeEntry;
+}
+
+interface SickLeaveEntry {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave: SickLeaveEntry;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
+
+export interface PatientEntry {
   id: string;
   name: string;
-  occupation: string;
+  dateOfBirth: string;
+  ssn: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  occupation: string;
+  entries: BaseEntry[]
 }
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn' | 'entries'>;
+
+export type NewPatientEntry = Omit<PatientEntry, 'id'>;
